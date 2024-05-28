0b97f8bfd9a3032d3c53d15b233cbcb33a61516c
Victor Bolivar De la Cruz <victor.bolivar@pucp.edu.pe>
2024-05-28 14:07:24 -0500

    bugfix(6.2.7+6.1.13+6.1.14+4.2.3): Improves security of specified tasks (#146)
    bugfix(6.2.7+6.1.13+6.1.14+4.2.3): Improves security of specified tasks.
    
    ---------
    Co-authored-by: Victor Bolivar <vbolivar@whitestack.com>

--- files/6_2_7.sh
+++ files/6_2_7.sh
@@ -3,13 +3,13 @@ grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "'"$(which nolo
     if [ ! -d "$dir" ]; then
         echo "The home directory ($dir) of user $user does not exist."
     else
-        for file in $dir/.[A-Za-z0-9]*; do
-            if [ ! -h "$file" -a -f "$file" ]; then
+        find $dir -name '.*' -type f | while read file; do
+            if [ ! -h "$file" ]; then
                 fileperm=$(ls -ld $file | cut -f1 -d" ")
-                if [ $(echo $fileperm | cut -c6) != "-" ]; then
+                if [ $(echo "$fileperm" | cut -c6) != "-" ]; then
                     echo "Group Write permission set on file $file"
                 fi
-                if [ $(echo $fileperm | cut -c9) != "-" ]; then
+                if [ $(echo "$fileperm" | cut -c9) != "-" ]; then
                     echo "Other Write permission set on file $file"
                 fi
             fi
